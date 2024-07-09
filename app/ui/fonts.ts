862523693eb1541b7539099525d5e3344175e94a
Kevin Everrett <advancedeveloper24@gmail.com>
2024-07-09 18:43:56 -0400

    navMenu

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,9 @@
-import { Inter } from "next/font/google";
+import { Inter, Kenia, Mukta, Lusitana } from "next/font/google";
+
+export const kenia = Kenia({ subsets: ["latin"], weight: "400" });
+
+export const mukta = Mukta({ subsets: ["latin"], weight: "400" });
 
 export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({ subsets: ["latin"], weight: "400" });
